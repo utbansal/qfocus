32c4a88ab0b3452f6ac60dab69dd99f0dd9403b3
utkarsh <Utkarsh.bansal@infinxinc.com>
2020-04-16 01:22:11 +0530

    Sample project, to test integration with.

---
+++ sampleProject/src/main/java/com/qfocus/sample/SampleApplication.java
@@ -0,0 +1,29 @@
+package com.qfocus.sample;
+
+
+import java.time.ZoneId;
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+public class SampleApplication {
+
+	//users={test1:'test1',test2:'test2'}
+	//https://stackoverflow.com/questions/26275736/how-to-pass-a-mapstring-string-with-application-properties/26276729#26276729
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleApplication.class, args);
+	}
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		ZoneId zone = ZoneId.of("UTC");
+		TimeZone.setDefault(TimeZone.getTimeZone(zone));
+	}
+
+
+}
